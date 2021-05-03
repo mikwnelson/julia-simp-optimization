73d0136028bc2994f3485a7a98545ff0a0c7d0f7
mikwnelson <80423776+mikwnelson@users.noreply.github.com>
2021-05-03 16:13:57 -0500

    Update optimization_test.jl
    Change function input into NLopt to hopefully add additional input variables.

--- optimization_test.jl
+++ optimization_test.jl
@@ -3,17 +3,15 @@ include("SIMP-Optimization.jl")
 
 opt = Opt(:LD_MMA, (m+1)*(n+1))
 
-function av_temp(x::Vector, grad::Vector)
+function av_temp(x::Vector, grad::Vector, Q, p, m, n)
     if length(grad) > 0
         dfav = d_f_av(x,p,m,n)
-        for i = 1:((m+1)*(n+1))
-            grad[i] = dfav[i]
-        end
+        grad[:] = dfav[:]
     end
-        return f_av(x,Q,p,m,n)
+    return f_av(x,Q,p,m,n)
 end
 
-function por(x::Vector, grad::Vector)
+function por(x::Vector, grad::Vector, m, n)
     if length(grad) > 0
         for i = 1:((m+1)*(n+1))
             grad[i] = 1/((m+1)*(n+1))
@@ -22,12 +20,15 @@ function por(x::Vector, grad::Vector)
     return (1/((m+1)*(n+1)))*(ones(((m+1)*(n+1)),1)')*x
 end
 
-opt.min_objective = av_temp
+min_objective!(opt, (x,grad)->av_temp(x,grad,Q,p,m,n))
+
+inequality_constraint!(opt, (x,grad)->por(x,grad,m,n))
+
 opt.lower_bounds = 0
 opt.upper_bounds = 1
 
-opt.inequality_constraint = por
+opt.ftol_rel = 0.00001
 
-η = vec(Eta')
+η = convert(Vector, vec(Eta'))
 
-(minf,minx,ret) = optimize(opt, η)
+(minf,minx,ret) = optimize!(opt, η)

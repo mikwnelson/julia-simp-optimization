5a0db1daf7339543c6a6cf9e5ce3701d021c9084
mikwnelson <80423776+mikwnelson@users.noreply.github.com>
2021-05-05 10:10:46 -0500

    Update optimization_test.jl
    Various improvements made by Paul, and optimizations.

--- optimization_test.jl
+++ optimization_test.jl
@@ -1,28 +1,18 @@
-using NLopt, SparseArrays, LinearAlgebra
+using NLopt, SparseArrays, LinearAlgebra, LaTeXStrings, PyPlot
 
 ##########################
 ## Fixed Variable Input ##
 ##########################
 
-p = 1
+p = 1.0
 
-m = 3
+m = 100
 
-n = 3
-
-###########################################
-## Create Optimization Problem Structure ##
-## Using MMA with dimentions (m+1)*(n+1) ##
-###########################################
+n = 100
 
 opt = Opt(:LD_MMA, (m+1)*(n+1))
 
-#########################
-## Average Temperature ##
-## Objective Function  ##
-#########################
-
-function av_temp(η::Vector, grad::Vector, p, m, n)
+function av_temp(η::Vector, grad::Vector, p, m, n, xlen = 0.1, ylen = 0.1, k_0 = 1.0, k_p = 100.0)
     
     #######################
     ## Assemble K Matrix ##
@@ -34,10 +24,10 @@ function av_temp(η::Vector, grad::Vector, p, m, n)
     #dy = "delta y"
     dy = ylen/m
 
-    Eta = reshape(η,m+1,n+1)
+    η = reshape(η,m+1,n+1)
 
     #Define Conductivity Penalization Function for design parameters eta
-    k = k_0 .+ (k_p - k_0).*Eta.^p
+    k = k_0 .+ (k_p - k_0).*η.^p
 
     #Control Volumes are designated based on matrix-type coordinates, so that volume [i,j] is the control volume in the i-th row and j-th column from the upper left.
 
@@ -53,9 +43,9 @@ function av_temp(η::Vector, grad::Vector, p, m, n)
     #Initialize K matrix
     K = spzeros((m*n),(m*n))
 
-    #Number control volumes based on node coordinates, going row-by-row, for m rows and n columns
+    #Number control volumes based on node coordinates, going column-by-column, for m rows and n columns
     function cord2num(i,j,n)
-        cv_num = j + (i-1)*n
+        cv_num = i + (j-1)*m
         return cv_num
     end
 
@@ -99,7 +89,7 @@ function av_temp(η::Vector, grad::Vector, p, m, n)
     ######################
 
     #Solve KT = Q
-    T = K\Q[:]
+    T = K \ vec(Q)
 
     ###########################
     ## Gradient Computations ##
@@ -107,7 +97,7 @@ function av_temp(η::Vector, grad::Vector, p, m, n)
 
     if length(grad) > 0
 
-        d_f_av = zeros(m+1,n+1)
+        grad = reshape(grad, m+1, n+1)
 
         ############################
         ##    Compute λ vector    ##
@@ -120,7 +110,7 @@ function av_temp(η::Vector, grad::Vector, p, m, n)
         ## Create ∂k/∂η Matrix ##
         #########################
 
-        dk = (p * (k_p - k_0)) .* Eta.^(p-1)
+        dk = (p * (k_p - k_0)) .* η.^(p-1)
 
         ###########################
         ## Assemble ∂K/∂η Matrix ##
@@ -133,103 +123,84 @@ function av_temp(η::Vector, grad::Vector, p, m, n)
             ##     for each (i,j)    ##
             ###########################
 
-            DK = spzeros((m*n),(m*n))
+            dK = spzeros((m*n),(m*n))
 
             if 2 <= j <= n
                 for a = max(1,i-1):min(i,m)
-                    DK[cord2num(a,j,n),cord2num(a,j-1,n)]=-0.5*(dy/dx)
-                    DK[cord2num(a,j-1,n),cord2num(a,j,n)]=-0.5*(dy/dx)
+                    dK[cord2num(a,j,n),cord2num(a,j-1,n)]=-0.5*(dy/dx)
+                    dK[cord2num(a,j-1,n),cord2num(a,j,n)]=-0.5*(dy/dx)
                 end
             end
             if 2 <= i <= m
                 for b = max(1,j-1):min(j,n)
-                    DK[cord2num(i,b,n),cord2num(i-1,b,n)]=-0.5*(dx/dy)
-                    DK[cord2num(i-1,b,n),cord2num(i,b,n)]=-0.5*(dx/dy)
+                    dK[cord2num(i,b,n),cord2num(i-1,b,n)]=-0.5*(dx/dy)
+                    dK[cord2num(i-1,b,n),cord2num(i,b,n)]=-0.5*(dx/dy)
                 end
             end
             for a = max(1,i-1):min(i,m), b = max(1,j-1):min(j,m)
-                DK[cord2num(a,b,n),cord2num(a,b,n)]=-sum(DK[cord2num(a,b,n),:])
+                dK[cord2num(a,b,n),cord2num(a,b,n)]=-sum(dK[cord2num(a,b,n),:])
             end
             if iseven(m)
                 hm=m÷2
                 if j==1 && (hm ≤ i ≤ hm + 1)
-                    DK[cord2num(hm,1,n),cord2num(hm,1,n)]+=0.5*(dy/dx)
+                    dK[cord2num(hm,1,n),cord2num(hm,1,n)]+=0.5*(dy/dx)
                 end
                 if j==1 && (hm +1 ≤ i ≤ hm + 2)
-                    DK[cord2num(hm+1,1,n),cord2num(hm+1,1,n)]+=0.5*(dy/dx)
+                    dK[cord2num(hm+1,1,n),cord2num(hm+1,1,n)]+=0.5*(dy/dx)
                 end
             else
                 hm=m÷2+1
                 if j==1 && (hm ≤ i ≤ hm + 1)
-                    DK[cord2num(hm,1,n),cord2num(hm,1,n)]+=0.5*(dy/dx)
+                    dK[cord2num(hm,1,n),cord2num(hm,1,n)]+=0.5*(dy/dx)
                 end
             end
-
-            #############################
-            ## (∂K/∂k)*(∂k/∂η) = ∂K/∂η ##
-            #############################
-
-            DK = DK.*dk[i,j]
 
             ###########################
             ## Find Nonzero elements ##
             ##  of ∂K/∂η_{i,j} and   ##
             ## Assemble ∂f_av Matrix ##
             ###########################
-
+            grad[i,j] = 0.0
             A, B, Va = findnz(dK)
             for k = 1:nnz(dK)
                 a = A[k]
                 b = B[k]
                 v = Va[k]
-                d_f_av[i,j]+=λ[a]*v*T[b]
+                grad[i,j] += λ[a]*v*T[b]
             end
+
+            #############################
+            ## (∂K/∂k)*(∂k/∂η) = ∂K/∂η ##
+            #############################
+
+            grad[i,j] *= dk[i,j]
         end
-
-        ############################
-        ## Vectorize ∂f_av Matrix ##
-        ############################
-
-        ∂f_av = convert(Vector, vec((d_f_av)'))
-
-        #############################
-        ## Input Elements of ∂f_av ##
-        ##   into 'grad' vector    ##
-        #############################
-
-        grad[:] = dfav[:]
-
     end
 
     ##########################
     ## Compute f(T) = T_avg ##
     ##########################
 
-    f_avg = (1/(m*n))*(ones((m*n),1))'*T
+    f_avg = sum(T) / (m*n)
 
     ########################
     ## Debugging Messages ##
     ########################
 
-    println("fav = $fav\n", "grad = $grad\n") #Output for Debugging Purposes
+    # println("fav = $fav\n", "grad = $grad\n") #Output for Debugging Purposes
 
     return f_avg
 end
 
-##################################
-## Porosity Constraint Function ##
-##################################
-
 function por(x::Vector, grad::Vector, m, n)
     if length(grad) > 0
-        for i = 1:((m+1)*(n+1))
-            grad[i] = 1/((m+1)*(n+1))
-        end
+        grad .= 1.0
     end
-    con = (1/((m+1)*(n+1)))*(ones(((m+1)*(n+1)),1)')*x
-    println("con = $con\n", "grad = $grad\n") #Output for Debugging Purposes
+    con = sum(x) - 0.1 * (m+1) * (n+1)
+    # println("con = $con\n", "grad = $grad\n") #Output for Debugging Purposes
     return con
 end
+
 
 
 min_objective!(opt, (x,g) -> av_temp(x,g,p,m,n))
@@ -241,8 +212,22 @@ opt.upper_bounds = 1
 
 opt.xtol_rel = 1e-4
 
-η = 0.1 .*ones((m+1)*(n+1))
+#η = rand( (m+1)*(n+1) ) # 0.1 .*ones((m+1)*(n+1))
+#dη = 1e-3 * randn(size(η))
+#grad = similar(η)
 
+#av_temp(η, grad, p,m,n)
+#@show norm(av_temp(η+dη, [], p,m,n) - av_temp(η, [], p,m,n) - dot(grad, dη))
+
+#por(η, grad, m,n)
+#@show norm(por(η+dη, [], m,n) - por(η, [], m,n) - dot(grad, dη))
+
+#nothing
+
+η = 0.1 .*ones((m+1)*(n+1))
 (minf,minx,ret) = optimize!(opt, η)
 numevals = opt.numevals # the number of function evaluations
 println("got $minf at $minx after $numevals iterations (returned $ret)")
+
+η = reshape(minx, m+1, n+1)
+F = imshow(η, extent = (0.0, 0.1, 0.0, 0.1))
